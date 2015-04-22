93d3bcb262ba2d85114d3b6be9887d92db44a833
MMX <trudosm@plu.edu>
2015-04-21 19:08:22 -0700

    Javadocs/Properties
    Created the Javadocs for the LoginAction file and created a resources
    folder with an ApplicationResources.properties file. Added the input
    into this file that the tutorial suggested.

--- src/net/viralpatel/struts2/LoginAction.java
+++ src/net/viralpatel/struts2/LoginAction.java
@@ -4,6 +4,13 @@ public class LoginAction {
 	private String username;
 	private String password;
 	
+	/**
+	 * This method authenticates the user.
+	 * This simple app checks to make sure the username is
+	 * admin and the password is admin123.
+	 * @return	Returns the string indicating 
+	 * authentication success or failure.
+	 */
 	public String execute()
 	{
 		if (this.username.equals("admin") && this.password.equals("admin123"))
@@ -14,21 +21,39 @@ public class LoginAction {
 		}
 	}
 	
+	/**
+	 * Retrieves the value stored in username.
+	 * 
+	 * @return	Returns the value of the private variable username.
+	 */
 	public String getUsername()
 	{
 		return username;
 	}
 	
+	/**
+	 * Sets the value of the the private variable username.
+	 * @param username	The new value used to set the current username.
+	 */
 	public void setUsername( String username )
 	{
 		this.username = username;
 	}
 	
+	/**
+	 * Retrieves the value stored in password.
+	 * 
+	 * @return	Returns the value of the private variable password.
+	 */
 	public String getPassword()
 	{
 		return password;
 	}
 	
+	/**
+	 * Sets the value of the the private variable password.
+	 * @param password	The new value used to set the current password.
+	 */
 	public void setPassword( String password )
 	{
 		this.password = password;
